6651fe4c30ebb4001fd79ef65dc677f6b363cf0c
rdlRisus <98750793+rdlRisus@users.noreply.github.com>
2023-02-25 22:38:27 +0300

    TT

--- 150-wm-rice.sh
+++ 150-wm-rice.sh
@@ -13,6 +13,6 @@ mv ./.config/* $HOME/
 sudo chmod 777 $HOME/.config/bspwm/bspwmrc
 sudo chmod 777 $HOME/.config/sxhkd/sxhkdrc
 sudo chmod 777 $HOME/.config/polybar/polybar.sh
-
+betterlockscreen -u $HOME/.wall/wallhaven-vmd693_1920x1080.png
 
 
